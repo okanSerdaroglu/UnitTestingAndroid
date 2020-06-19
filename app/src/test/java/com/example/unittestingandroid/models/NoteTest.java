bfe2f629ef88fb1ebd71cb7e184b10d737f08b20
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-06-19 19:58:03 +0300

    NoteTest template editted

---
+++ app/src/test/java/com/example/unittestingandroid/models/NoteTest.java
@@ -0,0 +1,28 @@
+package com.example.unittestingandroid.models;
+
+import org.junit.jupiter.api.Test;
+
+public class NoteTest {
+
+    // compare two equal notes
+
+    @Test
+    void isNoteEqualIdenticalProperties() throws Exception {
+
+        // Arrange
+
+        // Act
+
+        // Assert
+
+    }
+
+
+    // compare notes with 2 different ids
+
+    // Comparing notes with different timestamps
+
+    // Compare two notes with different titles
+
+    // Compare two notes with different content
+}
