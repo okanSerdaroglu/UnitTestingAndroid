76c946b2ced036c1a32da56fcedd1788edd90fab
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-06-20 14:45:29 +0300

    Room DAO Testing Part 1 ( Scenarios )

---
+++ app/src/androidTest/java/com/example/unittestingandroid/NoteDaoTest.java
@@ -0,0 +1,13 @@
+package com.example.unittestingandroid;
+
+public class NoteDaoTest extends NoteDatabaseTest {
+
+    // Insert, Read, Delete
+
+    // Insert, Read, Update, Read, Delete
+
+    // Insert note with null title, throw exception @NonNull
+
+    // Insert, Update with null title, throw exception
+
+}
