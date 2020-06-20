81a242dd29279b00e454986bff9b2e23f3ef0606
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-06-20 11:57:55 +0300

    Room Persistence Setup

--- app/src/main/java/com/example/unittestingandroid/di/AppModule.java
+++ app/src/main/java/com/example/unittestingandroid/di/AppModule.java
@@ -1,7 +1,36 @@
 package com.example.unittestingandroid.di;
 
+import static com.example.unittestingandroid.persistence.NoteDatabase.DATABASE_NAME;
+
+import android.app.Application;
+
+import androidx.room.Room;
+
+import com.example.unittestingandroid.persistence.NoteDao;
+import com.example.unittestingandroid.persistence.NoteDatabase;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 class AppModule {
+
+    @Singleton
+    @Provides
+    static NoteDatabase provideNoteDatabase(Application application) {
+        return Room.databaseBuilder(
+                application,
+                NoteDatabase.class,
+                DATABASE_NAME
+        ).build();
+    }
+
+    @Singleton
+    @Provides
+    static NoteDao provideNoteDao(NoteDatabase noteDatabase) {
+        return noteDatabase.getNoteDao();
+    }
+
 }
---
+++ app/src/main/java/com/example/unittestingandroid/persistence/NoteDao.java
@@ -0,0 +1,31 @@
+package com.example.unittestingandroid.persistence;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.unittestingandroid.models.Note;
+
+import java.util.List;
+
+import io.reactivex.Single;
+
+@Dao
+public interface NoteDao {
+
+    @Insert
+    Single<Long> insertNote(Note note) throws Exception;
+
+    @Query("SELECT * FROM notes")
+    LiveData<List<Note>> getNotes();
+
+    @Delete
+    Single<Integer>deleteNote(Note note);
+
+    @Update
+    Single<Integer> updateNote(Note note) throws Exception;
+
+}
