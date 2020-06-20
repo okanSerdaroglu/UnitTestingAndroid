88f8bbebb8139131115017efb838f0d5a7b734e6
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-06-20 13:19:41 +0300

    Testing a Room Database ( Instrumentation )

---
+++ app/src/androidTest/java/com/example/unittestingandroid/NoteDatabaseTest.java
@@ -0,0 +1,36 @@
+package com.example.unittestingandroid;
+
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+
+import com.example.unittestingandroid.persistence.NoteDao;
+import com.example.unittestingandroid.persistence.NoteDatabase;
+
+import org.junit.After;
+import org.junit.Before;
+
+public abstract class NoteDatabaseTest {
+
+    // system under test
+    private NoteDatabase noteDatabase;
+
+    public NoteDao getNoteDao() {
+        return noteDatabase.getNoteDao();
+    }
+
+    @Before
+    public void init() {
+        // mock database for testing
+        noteDatabase = Room.inMemoryDatabaseBuilder(
+                ApplicationProvider.getApplicationContext(),
+                NoteDatabase.class
+        ).build();
+    }
+
+    @After
+    public void finish() {
+        noteDatabase.close();
+    }
+
+
+}
