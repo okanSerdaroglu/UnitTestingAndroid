eae33e2fa2b5070ff30e2ca5319121e2294ea54c
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-06-23 14:25:16 +0300

    Note InsertUpdateHelper Class

--- app/src/main/java/com/example/unittestingandroid/ui/note/NoteViewModel.java
+++ app/src/main/java/com/example/unittestingandroid/ui/note/NoteViewModel.java
@@ -74,7 +74,39 @@ public class NoteViewModel extends ViewModel {
         }
         cancelInsertTransaction();
 
-        return null;
+        return new NoteInsertUpdateHelper<Integer>() {
+            @Override
+            public void setNoteId(int noteId) {
+                isNewNote = false;
+                Note currentNote = note.getValue();
+                currentNote.setId(noteId);
+                note.setValue(currentNote);
+            }
+
+            @Override
+            public LiveData<Resource<Integer>> getAction() throws Exception {
+                if (isNewNote) {
+                    return insertNote();
+                } else {
+                    return updateNote();
+                }
+            }
+
+            @Override
+            public String defineAction() {
+                if (isNewNote) {
+                    return ACTION_INSERT;
+                } else {
+                    return ACTION_UPDATE;
+                }
+            }
+
+            @Override
+            public void onTransactionComplete() {
+                updateSubscription = null;
+                insertSubscription = null;
+            }
+        }.getAsLiveData();
     }
 
     private boolean shouldAllowSave() {
